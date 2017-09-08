7cba16ec3ef2377fdbdd9db72f01562c28ad04b9
John Kim <jkjohnwuhan@gmail.com>
2017-09-08 17:08:46 +1000

    Text commit

--- app/src/main/java/com/co2017gmail/bilibili/smarttimer/home_screen.java
+++ app/src/main/java/com/co2017gmail/bilibili/smarttimer/home_screen.java
@@ -12,12 +12,5 @@ public class home_screen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
 
-        //TextView Date
-        TextView tvDisplayDate = (TextView) findViewById(R.id.tvDate);
-        long date = System.currentTimeMillis();
-
-        SimpleDateFormat sdf = new SimpleDateFormat("MMM MM dd, yyyy h:mm a");
-        String dateString = sdf.format(date);
-        tvDisplayDate.setText(dateString);
     }
 }
