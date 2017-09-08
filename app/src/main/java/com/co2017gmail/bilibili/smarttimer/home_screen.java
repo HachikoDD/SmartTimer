a1a66440beae1a7ccd5f6783bc3fb491704697c1
zhengyu1310 <zhengyu1310@gmail.com>
2017-09-08 15:35:44 +1000

    test branch

--- app/src/main/java/com/co2017gmail/bilibili/smarttimer/home_screen.java
+++ app/src/main/java/com/co2017gmail/bilibili/smarttimer/home_screen.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class home_screen extends AppCompatActivity {
-
+//test schedule branch
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
