3ea854d6712d975201f06b5646310d61f51b51ca
zhengyu1310 <zhengyu1310@gmail.com>
2017-09-08 15:41:45 +1000

    test branch 2

--- app/src/main/java/com/co2017gmail/bilibili/smarttimer/home_screen.java
+++ app/src/main/java/com/co2017gmail/bilibili/smarttimer/home_screen.java
@@ -9,5 +9,6 @@ public class home_screen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
-    }
+    }//test 2
+    
 }
