c2c43e16ae28d18e528270b955cec2093d0ccac8
John Kim <jkjohnwuhan@gmail.com>
2017-09-08 17:06:26 +1000

    Added TextView Date

--- app/src/main/java/com/co2017gmail/bilibili/smarttimer/home_screen.java
+++ app/src/main/java/com/co2017gmail/bilibili/smarttimer/home_screen.java
@@ -2,6 +2,8 @@ package com.co2017gmail.bilibili.smarttimer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import java.text.SimpleDateFormat;
 
 public class home_screen extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class home_screen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
+
+        //TextView Date
+        TextView tvDisplayDate = (TextView) findViewById(R.id.tvDate);
+        long date = System.currentTimeMillis();
+
+        SimpleDateFormat sdf = new SimpleDateFormat("MMM MM dd, yyyy h:mm a");
+        String dateString = sdf.format(date);
+        tvDisplayDate.setText(dateString);
     }
 }
