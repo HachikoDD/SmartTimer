999d3da069ed74cf17d69dacc99e82a22420696b
NamKyu Kim <jkjohnwuhan@gmail.com>
2017-09-14 17:37:33 +1000

    Added countdown function

--- app/src/main/java/com/co2017gmail/bilibili/smarttimer/home_screen.java
+++ app/src/main/java/com/co2017gmail/bilibili/smarttimer/home_screen.java
@@ -3,22 +3,71 @@ package com.co2017gmail.bilibili.smarttimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.os.Handler;
+import android.view.View;
 
+import java.util.Date;
 import java.text.SimpleDateFormat;
 
 public class home_screen extends AppCompatActivity {
+
+    private TextView txtTimerHour, txtTimerMinute, txtTimerSecond;
+    private TextView tvEvent;
+    private Handler handler;
+    private Runnable runnable;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
 
-        //TextView Date
-        TextView tvDisplayDate = (TextView) findViewById(R.id.tvDate);
-        long date = System.currentTimeMillis();
+        txtTimerHour = (TextView) findViewById(R.id.tv_timer_hour);
+        txtTimerMinute = (TextView) findViewById(R.id.tv_timer_minute);
+        txtTimerSecond = (TextView) findViewById(R.id.tv_timer_second);
+        tvEvent = (TextView) findViewById(R.id.tvEvent);
 
-        SimpleDateFormat sdf = new SimpleDateFormat("MMM MM dd");
-        String dateString = sdf.format(date);
-        tvDisplayDate.setText(dateString);
+        countDownStart();
+
+    }
+
+    public void countDownStart() {
+        handler = new Handler();
+
+        runnable = new Runnable() {
+            @Override
+            public void run() {
+                handler.postDelayed(this,1000);
+                try{
+                    SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+
+                    // Only in this format //YYYY-MM-DD
+                    Date futureDate = dateFormat.parse("2018-12-25");
+                    Date currentDate = new Date();
+
+                    if (!currentDate.after(futureDate)) {
+                        long diff = futureDate.getTime()
+                                - currentDate.getTime();
+                        long hours = diff / (60 * 60 * 1000);
+                        diff -= hours * (60 * 60 * 1000);
+                        long minutes = diff / (60 * 1000);
+                        diff -= minutes * (60 * 1000);
+                        long seconds = diff / 1000;
+
+                        txtTimerHour.setText("" + String.format("%02d", hours));
+                        txtTimerMinute.setText(""
+                                + String.format("%02d", minutes));
+                        txtTimerSecond.setText(""
+                                + String.format("%02d", seconds));
+                    } else {
+                        tvEvent.setVisibility(View.VISIBLE);
+                        tvEvent.setText("Time OUT!");
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        handler.postDelayed(runnable, 1 * 1000);
     }
 }
