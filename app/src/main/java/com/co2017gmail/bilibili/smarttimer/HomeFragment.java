0909cb8e45ecef0287a8f49f88a5c48caa260ce5
Dean_Zhang <zdmwj123@gmail.com>
2017-09-22 18:04:03 +1000

    dimen file

--- app/src/main/java/com/co2017gmail/bilibili/smarttimer/HomeFragment.java
+++ app/src/main/java/com/co2017gmail/bilibili/smarttimer/HomeFragment.java
@@ -1,18 +1,23 @@
 package com.co2017gmail.bilibili.smarttimer;
 
 
+import android.icu.text.SimpleDateFormat;
+import android.icu.util.ULocale;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.github.sundeepk.compactcalendarview.CompactCalendarView;
+
+import java.util.Locale;
+
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class HomeFragment extends Fragment {
-
 
     public HomeFragment() {
         // Required empty public constructor
