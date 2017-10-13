f16944bfb81672330cce2ae75edb5f7f9de128a9
Irene <amao8747@uni.sydney.edu.au>
2017-10-13 15:33:25 +1100

    me changed

--- app/src/main/java/com/co2017gmail/bilibili/smarttimer/me.java
+++ app/src/main/java/com/co2017gmail/bilibili/smarttimer/me.java
@@ -6,13 +6,22 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.provider.MediaStore;
+import android.net.Uri;
 
 
 /**
  * Created by Irene on 6/10/17.
  */
 
-public class me extends AppCompatActivity {
+public class me extends AppCompatActivity implements View.OnClickListener {
+
+    private static final int RESULT_LOAD_IMAGE = 1;
+    ImageView profile;
+    //Button upload;
 
 
     @Override
@@ -43,5 +52,31 @@ public class me extends AppCompatActivity {
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.BottomNavigation);
         navigation.setSelectedItemId(R.id.me);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        profile = (ImageView) findViewById(R.id.profile);
+        //upload = (Button) findViewById(R.id.upload);
+
+        profile.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.profile:
+                Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(galleryIntent, RESULT_LOAD_IMAGE);
+                break;
+        }
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && data != null){
+            Uri selectedImage = data.getData();
+            profile.setImageURI(selectedImage);
+        }
+
+
     }
 }
