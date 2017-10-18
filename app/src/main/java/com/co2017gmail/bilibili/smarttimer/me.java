fca5cfbf0734a6c6550d62f912494ca3da377c22
Dean_Zhang <zdmwj123@gmail.com>
2017-10-18 18:02:38 +1100

    select apps v3

--- app/src/main/java/com/co2017gmail/bilibili/smarttimer/me.java
+++ app/src/main/java/com/co2017gmail/bilibili/smarttimer/me.java
@@ -10,12 +10,15 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.CompoundButton;
 import android.widget.ImageView;
 import android.provider.MediaStore;
 import android.net.Uri;
 import android.database.sqlite.SQLiteDatabase;
 import android.widget.SeekBar;
+import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -30,6 +33,15 @@ public class me extends AppCompatActivity implements View.OnClickListener {
 
     private static final int RESULT_LOAD_IMAGE = 1;
     private static SeekBar seekBar;
+    private static TextView username ;
+    private static TextView email;
+    private static TextView phoneNumber;
+    private static int dailyUsageLimit;
+    private static Switch notificationSwitch;
+    private static Switch restrictionSwitch;
+    private static Switch wedgetSwitch;
+    private static Context context;
+
     private static TextView textView;
     UserDB userDB;
     ImageView profile;
@@ -38,6 +50,79 @@ public class me extends AppCompatActivity implements View.OnClickListener {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_me);
+        context = getApplicationContext();
+        username = (TextView) findViewById(R.id.username);
+        final String user_name = username.getText().toString();
+        notificationSwitch = (Switch) findViewById((R.id.switch_notif));
+        restrictionSwitch = (Switch) findViewById(R.id.switch_restr);
+        wedgetSwitch = (Switch) findViewById(R.id.switch_widget);
+
+        if(userDB.find(context,user_name)==null){
+            User user = new User();
+            user.userName = user_name;
+            user.notificationSwitch = false;
+            user.restrictionSwitch =false;
+            user.wedgetSwitch = false;
+            userDB.insert(context,user);
+            notificationSwitch.setChecked(false);
+            restrictionSwitch.setChecked(false);
+            wedgetSwitch.setChecked(false);
+        }else{
+            notificationSwitch.setChecked(UserDB.find(context,user_name).notificationSwitch);
+            restrictionSwitch.setChecked(UserDB.find(context,user_name).restrictionSwitch);
+            wedgetSwitch.setChecked(UserDB.find(context,user_name).wedgetSwitch);
+        }
+        final User user = userDB.find(context,user_name);
+        notificationSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(b){
+                    user.notificationSwitch = true;
+//                    Toast.makeText(me.this,"Ture",Toast.LENGTH_SHORT).show();
+                    UserDB.update(context,user);
+                }
+                else{
+                    user.notificationSwitch = false;
+//                    Toast.makeText(me.this,"False",Toast.LENGTH_SHORT).show();
+                    UserDB.update(context,user);
+                }
+            }
+        });
+
+        restrictionSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(b){
+                    user.restrictionSwitch = true;
+//                    Toast.makeText(me.this,"Ture",Toast.LENGTH_SHORT).show();
+                    UserDB.update(context,user);
+                }
+                else{
+                    user.restrictionSwitch = false;
+//                    Toast.makeText(me.this,"False",Toast.LENGTH_SHORT).show();
+                    UserDB.update(context,user);
+                }
+            }
+        });
+
+        wedgetSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(b){
+                    user.wedgetSwitch = true;
+//                    Toast.makeText(me.this,"Ture",Toast.LENGTH_SHORT).show();
+                    UserDB.update(context,user);
+                }
+                else{
+                    user.wedgetSwitch = false;
+//                    Toast.makeText(me.this,"False",Toast.LENGTH_SHORT).show();
+                    UserDB.update(context,user);
+                }
+            }
+        });
+
 
         if(savedInstanceState == null){
             getFragmentManager().beginTransaction()
@@ -45,6 +130,8 @@ public class me extends AppCompatActivity implements View.OnClickListener {
                     .commit();
         }
         seekbar();
+
+
 
         BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
                 = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -71,7 +158,6 @@ public class me extends AppCompatActivity implements View.OnClickListener {
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
         profile = (ImageView) findViewById(R.id.profile);
-        //upload = (Button) findViewById(R.id.upload);
 
         profile.setOnClickListener(this);
 
@@ -89,27 +175,36 @@ public class me extends AppCompatActivity implements View.OnClickListener {
 
     public void seekbar(){
         seekBar = (SeekBar) findViewById(R.id.limited_time_seek_bar);
+        final String user_name = username.getText().toString();
+        final User user = UserDB.find(context,user_name);
         seekBar.setMax(180);
+        seekBar.setProgress(user.dailyUsageLimit);
         textView = (TextView) findViewById(R.id.time_to_show);
-
+        username = (TextView) findViewById(R.id.username);
+        int progress = user.dailyUsageLimit;
+        textView.setText(progress+"/"+seekBar.getMax());
+        textView.setTextColor(getTextColor(progress));
         seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            int progress_valule;
-
+            int progress_valule = user.dailyUsageLimit;
             @Override
             public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
                 progress_valule = i;
-
                 textView.setText(progress_valule+"/"+seekBar.getMax());
+                textView.setTextColor(getTextColor(progress_valule));
             }
 
             @Override
             public void onStartTrackingTouch(SeekBar seekBar) {
                 textView.setText(progress_valule+"/"+seekBar.getMax());
+                textView.setTextColor(getTextColor(progress_valule));
             }
 
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
                 textView.setText(progress_valule+"/"+seekBar.getMax());
+                textView.setTextColor(getTextColor(progress_valule));
+                user.dailyUsageLimit = progress_valule;
+                UserDB.update(context,user);
             }
         });
 
@@ -123,4 +218,16 @@ public class me extends AppCompatActivity implements View.OnClickListener {
         }
     }
 
+    private  int getTextColor(int progress){
+        int result;
+        if(0<= progress && progress <60){
+            result = getResources().getColor(R.color.green);
+        }
+        else if(60<= progress && progress <120){
+            result = getResources().getColor(R.color.yellow);
+        }
+        else
+            result = getResources().getColor(R.color.red);
+        return  result;
+    }
 }
